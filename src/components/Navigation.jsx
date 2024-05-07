0fea931bb383d2da92b6f4ccfc318eacf7ec8d19
Matthew Seneres <mattseneris18@gmail.com>
2024-05-07 12:40:09 +0800

    added burger icon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import NavigationCSS from "../styles/Navigation.module.css";
 
 export default function Navigation() {
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+  function handleClick() {
+    setIsNavOpen((prevValue) => !prevValue);
+  }
+
   return (
     <nav className={NavigationCSS.container}>
+      <h1 className={NavigationCSS.logo}>HAUSIFY</h1>
 
-      <h1 className={NavigationCSS.logo}>
-        HAUSIFY
-      </h1>
+      <div className={NavigationCSS.burgerIcon}>
+        <label className={NavigationCSS.burger} for="burger">
+          <input
+            onClick={handleClick}
+            className={NavigationCSS.line}
+            type="checkbox"
+            id="burger"
+          />
+        </label>
+      </div>
 
-      <ul className={NavigationCSS.list}>
+      <ul
+        className={`${NavigationCSS.list} 
+        ${isNavOpen ? NavigationCSS.list_open : NavigationCSS.list_closed}`}
+      >
         <li className={NavigationCSS.item}>
           <a href=""> custom builds </a>
         </li>
@@ -19,6 +36,7 @@ export default function Navigation() {
         <li className={NavigationCSS.item}>
           <a href=""> contact us </a>
         </li>
+
         <li className={NavigationCSS.item}>
           <a href="">register</a>
         </li>
