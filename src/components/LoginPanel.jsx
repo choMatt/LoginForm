45875c09d931b81fb39766281120b713120fa3d7
Matthew Seneres <mattseneris18@gmail.com>
2024-05-08 20:04:40 +0800

    added new component Login Panel

--- src/components/LoginPanel.jsx
+++ src/components/LoginPanel.jsx
@@ -1,9 +1,49 @@
-import React from "react"
+import { useState } from 'react'
+import React from "react";
+import LoginPanelCSS from "../styles/LoginPanel.module.css";
 
-export default function LoginPanel(){
-    return(
-        <section>
+export default function LoginPanel() {
+  const { container, title } = LoginPanelCSS;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
 
-        </section>
-    )
+  function handleChange(event){
+    const { value, name } = event.target
+    setFormData((prevFormData) => {
+        return {
+            ...prevFormData,
+            [name] : value
+        }
+    })
+  }
+
+  return (
+    <section className={container}>
+      <h1 className={title}>building your dream home</h1>
+
+      <form>
+        <label htmlFor="email">Email Address:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+        <input type="submit" value="Submit" />
+
+        <button>create an account</button>
+      </form>
+    </section>
+  );
 }
